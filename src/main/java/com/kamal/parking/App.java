5e231b5277460f2010f240baf497c74e823350e4
kamalesh2602 <kamaleshg2602@gmail.com>
2026-03-02 21:34:14 +0530

    Initial Maven setup

---
+++ src/main/java/com/kamal/parking/App.java
@@ -0,0 +1,13 @@
+package com.kamal.parking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
